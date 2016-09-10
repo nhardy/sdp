5d75ba67b23b9cd8b448cc1cb5e6efb0ea2a64dc
Nathan Hardy <nhardy@live.com.au>
2016-09-10 16:23:43 +1000

    More UTS branding

--- src/app/components/SiteFooter/index.js
+++ src/app/components/SiteFooter/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import cx from 'classnames';
+
+import FontAwesome from 'app/components/FontAwesome';
 
 import styles from './styles.styl';
 
@@ -9,22 +12,20 @@ const SiteFooter = () => (
       <div className={styles.wrapper}>
         <div className={styles.info}>
           <h3 className={styles.heading}>
-            <a className={styles.link} href="https://www.uts.edu.au/about" target="_blank">About Us</a>
+            <a className={styles.headingLink} href="https://www.uts.edu.au/about" target="_blank">About Us</a>
           </h3>
           <ul>
             <li><a href="https://www.uts.edu.au/about/university" target="_blank">The University</a></li>
             <li><a href="https://www.uts.edu.au/about/maps-and-facilities" target="_blank">Maps and facilities</a></li>
             <li><a href="https://www.uts.edu.au/about/uts-governance" target="_blank">UTS governance</a></li>
             <li><a href="https://www.uts.edu.au/about/faculties" target="_blank">Faculties</a></li>
-          </ul>
-          <ul>
             <li><a href="https://www.uts.edu.au/about/utsweb-statements/privacy-statement" target="_blank">Privacy statement</a></li>
             <li><a href="https://www.uts.edu.au/about/utsweb-statements/disclaimer" target="_blank">Disclaimer</a></li>
           </ul>
         </div>
         <div className={styles.info}>
           <h3 className={styles.heading}>
-            <a className={styles.link} href="https://www.lib.uts.edu.au/" target="_blank">Library</a>
+            <a className={styles.headingLink} href="https://www.lib.uts.edu.au/" target="_blank">Library</a>
           </h3>
           <ul>
             <li><a href="https://www.uts.edu.au/library-catalogue" target="_blank">Search catalogue</a></li>
@@ -34,7 +35,7 @@ const SiteFooter = () => (
         </div>
         <div className={styles.info}>
           <h3 className={styles.heading}>
-            <a className={styles.link} href="https://www.uts.edu.au/uts-newsroom">Newsroom</a>
+            <a className={styles.headingLink} href="https://www.uts.edu.au/uts-newsroom">Newsroom</a>
           </h3>
           <ul>
             <li><a href="https://www.uts.edu.au/uts-events" target="_blank">Events</a></li>
@@ -44,20 +45,18 @@ const SiteFooter = () => (
         </div>
         <div className={styles.info}>
           <h3 className={styles.heading}>
-            <a className={styles.link} href="https://www.uts.edu.au/staff-connect" target="_blank">Staff</a>
+            <a className={styles.headingLink} href="https://www.uts.edu.au/staff-connect" target="_blank">Staff</a>
           </h3>
           <ul>
             <li><a href="https://staff.uts.edu.au/" target="_blank">Staff Connect</a></li>
             <li><a href="https://www.uts.edu.au/webmail" target="_blank">Webmail</a></li>
             <li><a href="https://www.uts.edu.au/staff-directory" target="_blank">Staff Directory</a></li>
-          </ul>
-          <ul>
             <li><a href="https://www.uts.edu.au/staff/jobs-uts/working-uts" target="_blank">Jobs at UTS</a></li>
           </ul>
         </div>
-        <div className={styles.info}>
-          <h3 className={styles.heading}>
-            <a className={styles.link} href="https://www.uts.edu.au/" target="_blank">University of Technology Sydney</a>
+        <div className={cx(styles.info, styles.double)}>
+          <h3 className={styles.lesserHeading}>
+            <a className={styles.headingLink} href="https://www.uts.edu.au/" target="_blank">University of Technology Sydney</a>
           </h3>
           <p>
             <strong>City campus</strong>
@@ -66,7 +65,36 @@ const SiteFooter = () => (
             <br />
             <span>Ultimo NSW 2007</span>
           </p>
-          <span>{(new Date()).getFullYear()}</span>
+        </div>
+        <div className={cx(styles.info, styles.full, styles.secondaryInfo)}>
+          <div>
+            <h3 className={styles.lesserHeading}>Telephone</h3>
+            <p>+61 2 9514 2000</p>
+          </div>
+          <div>
+            <h3 className={styles.lesserHeading}>Student inquiries</h3>
+            <p>ask.uts.edu.au</p>
+          </div>
+          <div>
+            <h3 className={styles.lesserHeading}>Postal address</h3>
+            <p>P.O. Box 123, Broadway, NSW 2007</p>
+          </div>
+          <div className={styles.social}>
+            <a className={styles.socialLink} href="http://facebook.com/UTSEngage"><FontAwesome className="fa-facebook-square" /></a>
+            <a className={styles.socialLink} href="http://twitter.com/utsengage"><FontAwesome className="fa-twitter" /></a>
+            <a className={styles.socialLink} href="http://instagram.com/utsengage"><FontAwesome className="fa-instagram" /></a>
+            <a className={styles.socialLink} href="https://www.youtube.com/user/utschannel"><FontAwesome className="fa-youtube-play" /></a>
+            <a className={styles.socialLink} href="http://linkedin.com/company/166678"><FontAwesome className="fa-linkedin-square" /></a>
+            <a className={styles.link} href="http://www.uts.edu.au/about/contacts/uts-contacts">See all contacts</a>
+          </div>
+        </div>
+        <div className={cx(styles.info, styles.full, styles.footnote)}>
+          <p>
+            © Copyright {(new Date()).getFullYear()} 500DollarHdWinna
+            <br />
+            The page is authorised by Money Bags
+          </p>
+          <a className={styles.github} href="https://www.github.com/nhardy/sdp"><FontAwesome className="fa-github" /></a>
         </div>
       </div>
     </div>
