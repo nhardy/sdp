a3ac47349788652605b9ccbf07024cca83805477
Nathan Hardy <nhardy@live.com.au>
2016-10-20 12:42:01 +1100

    Faux-validation for attendance marking

--- src/app/views/Attendance/index.js
+++ src/app/views/Attendance/index.js
@@ -7,6 +7,8 @@ import Helmet from 'react-helmet';
 import DefaultLayout from 'app/layouts/Default';
 import Form from 'app/components/Form';
 
+import styles from './styles.styl';
+
 
 // Mock attendance marking
 @connect(null, {})
@@ -16,19 +18,39 @@ export default class AttendanceView extends Component {
     router: routerShape,
   };
 
-  submit = () => {
-    this.props.router.push('/bookings');
+  state = {
+    error: null,
+  };
+
+  onChange = () => this.setState({ error: null });
+
+  submit = async (e) => {
+    e.preventDefault();
+    const code = this._code.value;
+    await new Promise(resolve => setTimeout(resolve, 500));
+    if (code !== 'thecode') {
+      this.setState({ error: 'Invalid code' });
+    } else {
+      this.props.router.push('/bookings');
+    }
+    return false;
   };
 
   render() {
+    const { error } = this.state;
     return (
       <DefaultLayout>
         <Helmet title="Attendance | UTS: HELPS Booking System" />
         <Form>
           <h1>Attendance</h1>
+          {error && (
+            <div className={styles.error}>
+              <span>{error}</span>
+            </div>
+          )}
           <label htmlFor="code">Attendance code</label>
-          <input type="text" id="code" name="code" autoFocus />
-          <input type="button" value="Submit" onClick={this.submit} />
+          <input type="text" id="code" ref={ref => (this._code = ref)} onChange={this.onChange} autoFocus />
+          <input type="submit" value="Submit" onClick={this.submit} />
         </Form>
       </DefaultLayout>
     );
